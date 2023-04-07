7e53c1d8c1a72a8380ec67def18770928ed536fd
Ana <mihalceanuana@gmail.com>
2023-04-07 12:08:28 +0200

    Add improved scripts

---
+++ scripts/print_pss.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# This script is from Poonam Parhar's article https://poonamparhar.github.io/troubleshooting_native_memory_leaks/
+pid=1
+echo 'Monitoring PSS of the Process:' $pid
+while true
+do
+   cat /proc/$pid/smaps | grep -i 'Pss:' |  awk '{Total+=$2} END {print Total}'
+  sleep 10
+done
+
--- scripts/track.sh
+++ scripts/track.sh
@@ -3,6 +3,9 @@
 echo "jcmd 1 VM.native_memory baseline"
 docker exec -it -d springboot-todo-service sh -c "jcmd 1 VM.native_memory baseline"
 
+echo "Wait 10 seconds"
+sleep 10
+
 read -p "Run a diff? " -n 1 -r
 echo    # (optional) move to a new line
 if [[  $REPLY =~ ^[Yy]$ ]]
